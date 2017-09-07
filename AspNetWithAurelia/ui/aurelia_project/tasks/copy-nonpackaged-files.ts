6c3445dbc64e3644716908b8db4ec02557f5e023
Dave Warry <dave@warry.org.uk>
2017-09-07 22:02:35 +0100

    Bundle popper.js.
    This only seems to work if it's the first item in the 'prepend' section. Bah.

--- AspNetWithAurelia/ui/aurelia_project/tasks/copy-nonpackaged-files.ts
+++ AspNetWithAurelia/ui/aurelia_project/tasks/copy-nonpackaged-files.ts
@@ -7,10 +7,6 @@ export default function copyNonpackagedFiles() {
   const popper = "node_modules/popper.js";
   const source = 'node_modules/font-awesome';
 
-  const taskPopper = gulp.src(`${popper}/dist/umd/popper.min.js`)
-    .pipe(changedInPlace({ firstPass: true }))
-    .pipe(gulp.dest(`${project.platform.output}`));
-
   const taskCss = gulp.src(`${source}/css/font-awesome.min.css`)
     .pipe(changedInPlace({ firstPass: true }))
     .pipe(gulp.dest(`${project.platform.output}/css`));
